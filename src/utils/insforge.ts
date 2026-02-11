04f154d03e657174e6ed43f5c8b8c3156615c3e6
Michael Mwabonje Ringa <ringa.michael@gmail.com>
2026-02-11 22:25:57 +0300

    chore: switch to 'hosmanagement' project and update credentials

--- src/utils/insforge.ts
+++ src/utils/insforge.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@insforge/sdk';
 
-const INSFORGE_URL = 'https://gn9nuyhp.us-west.insforge.app';
-const INSFORGE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3OC0xMjM0LTU2NzgtOTBhYi1jZGVmMTIzNDU2NzgiLCJlbWFpbCI6ImFub25AaW5zZm9yZ2UuY29tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4MzU3MzR9.Yx4rTgiCj0x7wjJ-siN1-5ePqQCmNpnakU4qkVyO2vw';
+const INSFORGE_URL = 'https://p9ekmjgi.us-west.insforge.app';
+const INSFORGE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3OC0xMjM0LTU2NzgtOTBhYi1jZGVmMTIzNDU2NzgiLCJlbWFpbCI6ImFub25AaW5zZm9yZ2UuY29tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4Mzc3OTV9.FsOACPTqULV3CAb2VlS0yGsKm2X68KLaV5AYxIcVIvM';
 
 export const insforge = createClient({
     baseUrl: INSFORGE_URL,
